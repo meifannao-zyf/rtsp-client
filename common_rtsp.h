a19b1ca3e5726716b08335e6cfa5ba6508b4dbb3
meifannao-zyf <18829281479@163.com>
2019-03-04 15:02:26 +0800

    Add files via upload

---
+++ common_rtsp.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#ifdef RTSP_PARSE_EXPORT 
+#define RTSP_PARSE_API __declspec(dllexport)
+#else
+#define RTSP_PARSE_API __declspec(dllimport)
+#endif
+
+typedef enum ELive_RtspDataType
+{
+	LIVE_RTSP_DATA_TYPE_INVALID = -1,
+	LIVE_RTSP_DATA_TYPE_V,
+	LIVE_RTSP_DATA_TYPE_A
+}ELive_RtspDataType;
+
+typedef enum ELive_RtspVideoEncodeType
+{
+	LIVE_ENCODE_V_INVALID = -1,
+	LIVE_ENCODE_V_H264,
+	LIVE_ENCODE_V_MP2T
+}ELive_RtspVideoEncodeType;
+
+typedef enum ELive_RtspAudioEncodeType
+{
+	LIVE_ENCODE_A_INVAID = -1,
+	LIVE_ENCODE_A_PCMA,
+	LIVE_ENCODE_A_PCMU,
+	LIVE_ENCODE_A_AAC
+}ELive_RtspAudioEncodeType;
+
+typedef struct ELive_VideoParam
+{
+	ELive_RtspVideoEncodeType video_encode_type;
+	std::string sps_pps_ext;
+	bool is_i_frame;
+	__int64 pts;
+
+	ELive_VideoParam()
+	{
+		video_encode_type = LIVE_ENCODE_V_INVALID;
+		is_i_frame = false;
+		pts = 0;
+	}
+}ELive_VideoParam;
+
+typedef struct ELive_AudioParam
+{
+	ELive_RtspAudioEncodeType audio_encode_type;
+	int channels;
+	int samples_rate;
+	__int64 pts;
+
+	ELive_AudioParam()
+	{
+		audio_encode_type = LIVE_ENCODE_A_INVAID;
+		channels = 1;			//g711默认配置
+		samples_rate = 8000;	
+		pts = 0;
+	}
+}ELive_AudioParam;
+
+typedef struct SLive_RtspDataInfo
+{
+	ELive_RtspDataType data_type;					//数据类型
+	ELive_VideoParam video_param;
+	ELive_AudioParam audio_param;
+
+	SLive_RtspDataInfo()
+	{
+		data_type = LIVE_RTSP_DATA_TYPE_INVALID;
+	}
+
+}SLive_RtspDataInfo;
+
+typedef void (__stdcall *rtsp_data_callback)(unsigned char* data, int data_len, SLive_RtspDataInfo data_info, void* user_param);
---
+++ parse_rtsp.h
@@ -0,0 +1,40 @@
+#pragma once
+
+/* 私有头文件 */
+#include <string>
+#include <Windows.h>
+#include "common_rtsp.h"
+
+class RTSP_PARSE_API CClientMutex
+{
+public:
+	CClientMutex(){InitializeCriticalSection(&client_mutex_);}
+	~CClientMutex(){DeleteCriticalSection(&client_mutex_);}
+
+	__inline void get_mutex(){EnterCriticalSection(&client_mutex_);}
+	__inline void release_mutex(){LeaveCriticalSection(&client_mutex_);}
+
+private:
+
+	CRITICAL_SECTION client_mutex_;
+};
+
+class RTSP_PARSE_API CRTSPClient
+{
+public:
+	CRTSPClient();
+	~CRTSPClient();
+
+	void run(std::string url, rtsp_data_callback rtsp_data_cb, void* user_param);
+	void stop();
+
+	bool has_audio_stream();
+
+private:
+	static unsigned __stdcall open_rtsp_thread(void* param);
+
+	bool is_need_shutdown_stream_;
+	void* rtsp_live_client_;
+	char event_loop_execute_;
+	CClientMutex mutex_;
+};
